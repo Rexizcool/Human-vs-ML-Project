eb02365bf039ce0c7c493023873663b926ddf647
Rexizcool <s947182@student.knoxschools.org>
2026-02-24 21:30:23 +0000

    stufff

--- getting_started/your_work_here.py
+++ getting_started/your_work_here.py
@@ -1,12 +1,16 @@
- import pandas as pd
+import pandas as pd
 import matplotlib as plt
+import seaborn as sns
+
 
 penguin=pd.read_csv('/workspaces/Human-vs-ML-Project/data/penguins_size.csv')
 
+def make_plot(factor_1, factor_2):
+    factor_1_label = factor_1.replace('_', ' ')
+    factor_2_label = factor_2.replace('_', ' ')
+    
+    
 
 
-
-
-adl_peng_lgth = penguin.loc[penguin['species'] == "Adelie", ['culmen_length_mm']]
 
 
