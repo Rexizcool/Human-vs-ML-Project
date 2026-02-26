825010c438961608548799b8529246025b84df61
Rexizcool <s947182@student.knoxschools.org>
2026-02-26 20:23:53 +0000

    updated human classifier

--- human_algorithm/your_work_here.py
+++ human_algorithm/your_work_here.py
@@ -1,6 +1,7 @@
-# if culmen_length_mm >= 45 and island == 'Dream':
-#    species = 'Chinstap'
-# elif culmen_length_mm >= 45 and island == 'Biscoe':
-#    species = 'Gentoo'
-# elif culmen_length_mm < 45:
-#    species = 'Adelie'
+def human_classify2():
+    if culmen_length_mm >= 45 and island == 'Dream':
+        species = 'Chinstap'
+        elif culmen_length_mm >= 45 and island == 'Biscoe':
+        species = 'Gentoo'
+    elif culmen_length_mm < 45:
+        species = 'Adelie'
